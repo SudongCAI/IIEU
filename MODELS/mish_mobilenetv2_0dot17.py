da9ece2bb4db16b5851f9cf17109c8bb86fac3d7
SudongCAI <74641031+SudongCAI@users.noreply.github.com>
2023-11-09 02:58:28 +0900

    Update files

--- MODELS/mish_mobilenetv2_0dot17.py
+++ MODELS/mish_mobilenetv2_0dot17.py
@@ -198,5 +198,5 @@ def mish_mobilenetv2(**kwargs):
 @register_model
 def mish_mobilenetv2(pretrained=False, **kwargs):
     return build_model_with_cfg(
-        MobileNetV2(width_mult=0.17), 'mish_mobilenetv2', default_cfg=default_cfg['mobilenetv2'],
+        MobileNetV2, 'mish_mobilenetv2', default_cfg=default_cfg['mobilenetv2'],
         pretrained=pretrained, **kwargs)
